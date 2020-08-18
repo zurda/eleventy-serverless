14b2eebaa9bb1799e015f438deafb959a16b7fec
zurda <m.weizman@gmail.com>
2020-08-18 23:31:30 +0100

    Fetch api data and combine with local data
    Previously, the pokemons handler returned whatever was in the pokemons.json file in the data folder. This adds an api call to fetch pokemon data by id for each of the pokemons in the data file. It then combines the returned data with the existing pokemon fields.

--- functions/pokemons.js
+++ functions/pokemons.js
@@ -1,8 +1,27 @@
+const fetch = require('node-fetch');
 const pokemons = require('../data/pokemons.json');
 
 exports.handler = async () => {
+  const promises = pokemons.map((pokemon) => {
+    const api = `https://pokeapi.co/api/v2/pokemon/${pokemon.id}`;
+    return fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const { weight, height, abilities } = data;
+
+        return {
+          ...pokemon,
+          weight,
+          height,
+          abilities,
+        };
+      });
+  });
+
+  const pokemonsWithApiData = await Promise.all(promises);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(pokemons),
+    body: JSON.stringify(pokemonsWithApiData),
   };
 };
