71f0a35528cce615ef6dd30b0609a3e3cb8d29f5
Zurda <m.weizman@gmail.com>
2020-12-20 19:38:55 +0000

    Add netlify identity and restrict form access

--- functions/add-team-member.js
+++ functions/add-team-member.js
@@ -2,6 +2,16 @@ const { query } = require('./utils/hasura');
 
 exports.handler = async (event, context) => {
     const { member_name, team_name } = JSON.parse(event.body);
+    const { user } = context.clientContext;
+    const isLoggedIn = user && user.app_metadata && user.app_metadata.roles;
+    const roles = isLoggedIn ? user.app_metadata.roles : [];
+
+    if (!isLoggedIn || !roles.includes('admin')) {
+        return {
+        statusCode: 401,
+        body: 'Unauthorized',
+        };
+    }
 
     const result = await query({
         query: `
