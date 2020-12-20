b770a46bf58f25a0680bfb7dd9c91506998abc31
Zurda <m.weizman@gmail.com>
2020-12-20 15:34:01 +0000

    Swap id with name
    Prior to this change, we were fetching the pokemon data by id, which was also supplied by the api. This changes the data to fetch on pokemon name, since that is the joined key the pokemon api and teams have in common.

--- functions/pokemons.js
+++ functions/pokemons.js
@@ -6,7 +6,7 @@ exports.handler = async () => {
   const promises = pokemons.map((pokemon) => {
     const membersObj = teamMembers.filter((member) => member.team_name.toLowerCase() === pokemon.name.toLowerCase());
     const team_members = membersObj.map(member => member.name);
-    const api = `https://pokeapi.co/api/v2/pokemon/${pokemon.id}`;
+    const api = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
     return fetch(api)
       .then((response) => response.json())
       .then((data) => {
