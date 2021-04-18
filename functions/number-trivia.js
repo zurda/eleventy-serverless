abb8ae46115ee2bc08020d4b4c7d2463eac5d7d9
zurda <m.weizman@gmail.com>
2021-04-18 10:20:18 +0100

    Add function to get trivia for current count

---
+++ functions/number-trivia.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+const { query } = require('./utils/hasura');
+
+const getNumberTrivia = (num) => {
+
+    const api = `http://numbersapi.com/${num}/trivia?json`;
+    return fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        const { text } = data;
+
+        return {
+          trivia: text,
+          count: num,
+        };
+      });
+  };
+
+exports.handler = async () => {
+  const { team_members: teamMembers } = await query({
+    query: `
+      query {
+        team_members{
+          member_name
+        }
+      }
+    `,
+  });
+
+  const numberFact = await getNumberTrivia(teamMembers.length + 1);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(numberFact),
+  };
+};
